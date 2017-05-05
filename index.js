597850c50eebd1abb7a5dce77da7014068e972fb
Lee Hull <lhull@hcpci.com>
2017-05-05 11:07:48 -0400

    wrong version replaced

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
 var config = require('config');
 var Path = require('path');
-var fs = require('fs');
+var jetpack = require('fs-jetpack');
 
-get = function() {}
+getSecrets = function() {
+  var secretsPath = process.env.SECRET_PATH || '/run/secrets'
 
-getSecrets = function(CONFIG_DIR, extNames) {
+  var result = {};
+  var files = jetpack.list(secretsPath);
+  if (files) {
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      var contents = jetpack.read(`${secretsPath}/${file}`, 'utf8');
+      result[file] = contents;
+    }
+  }
+
+  return result;
+}
+
+parseSecretsAndEnv = function(CONFIG_DIR, extNames) {
   var result = {};
   extNames.forEach(function(extName) {
     var fullFilename = Path.join(CONFIG_DIR, 'custom-environment-variables' + '.' + extName);
     var configObj = config.util.parseFile(fullFilename);
     if (configObj) {
-      //var environmentSubstitutions = config.util.substituteDeep(configObj, process.env);
-      //config.util.extendDeep(result, environmentSubstitutions);
+      var secrets = this.getSecrets();
 
-      var environmentSubstitutions = config.util.substituteDeep(configObj, {PORT: '1000'});
+      var environmentSubstitutions = config.util.substituteDeep(configObj, secrets);
+      config.util.extendDeep(result, environmentSubstitutions);
+
+      var environmentSubstitutions = config.util.substituteDeep(configObj, process.env);
       config.util.extendDeep(result, environmentSubstitutions);
     }
   });
 
-  console.log(result)
   return result;
 };
 
 module.exports = function() {
-  config.util.getCustomEnvVars = getSecrets;
+  config.util.getCustomEnvVars = parseSecretsAndEnv;
 
-  config.util.loadFileConfigs();
+  var t = config;
+
+  // Merge configurations into this
+  config.util.extendDeep(t, config.util.loadFileConfigs());
+  config.util.attachProtoDeep(t);
+
+  // Perform strictness checks and possibly throw an exception.
+  config.util.runStrictnessChecks(t);
 
   return config;
-}
+}()
